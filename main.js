df0ea5a6f14992f5bb935199d9110d7957fe5b35
Chris Harvey <chris@chrisnharvey.com>
2015-12-09 22:18:23 +0000

    Adding main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// Main window is in the global scope to prevent the
+// object being deleted during garbage collection.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+app.on('ready', function() {
+    // Create the window.
+    mainWindow = new BrowserWindow;
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.maximize();
+    mainWindow.setFullScreen(true);
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Free up some memory
+        mainWindow = null;
+    });
+});
