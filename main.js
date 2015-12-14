ae07d31ced484434e2fb8984175ecabc0ba906a8
Chris Harvey <chris@chrisnharvey.com>
2015-12-14 00:36:44 +0000

    Moving files around

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.on('ready', function() {
     if (env == 'production') mainWindow.setMenu(null);
 
     // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/views/index.html');
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
