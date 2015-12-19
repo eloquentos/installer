6df3746d985ee24935e12a1d8c4d0f07fc7d4392
Chris Harvey <chris@chrisnharvey.com>
2015-12-19 23:37:51 +0000

    Replace blockutils with its npm counterpart

--- vendor/blockutils/examples/sample.js
+++
@@ -1,9 +0,0 @@
-var blk = require('../blockutils');
-
-blk.getBlockInfo({"onlyStandard": true}, function(err,json) {
-  if (err) {
-    console.log("ERROR: " + err);
-  } else {
-    console.log("OK: " + JSON.stringify(json,null,"  "));
-  }
-});
