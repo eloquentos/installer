27d9dca7034c0e7e20b16665f04aa769530b6db6
Chris Harvey <chris@chrisnharvey.com>
2015-12-20 22:06:27 +0000

    Adding gulp task for compiling swig templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var swig = require('gulp-swig');
 
 gulp.task('sass', function () {
   gulp.src('./assets/sass/main.scss')
@@ -7,6 +8,13 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./assets/css/'));
 });
 
+gulp.task('views', function() {
+  gulp.src('./views/**/*.html')
+    .pipe(swig())
+    .pipe(gulp.dest('./html/'))
+});
+
 gulp.task('watch', function () {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  gulp.watch('./views/**/*.html', ['views']);
 });
