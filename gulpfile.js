c7340468510a7594a95993ba7583201d6762e9f1
Chris Harvey <chris@chrisnharvey.com>
2015-12-23 22:37:00 +0000

    Removing swig

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var swig = require('gulp-swig');
 
 gulp.task('sass', function () {
   gulp.src('./assets/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css/'));
-});
-
-gulp.task('views', function() {
-  gulp.src('./views/**/*.html')
-    .pipe(swig())
-    .pipe(gulp.dest('./html/'))
 });
 
 gulp.task('watch', function () {
