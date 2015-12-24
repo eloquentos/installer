2f7823386ce391b263a9d484548ab21b063fbfb8
Chris Harvey <chris@chrisnharvey.com>
2015-12-24 00:34:00 +0000

    CoffeeScript compiler and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   gulp.src('./assets/sass/main.scss')
@@ -7,7 +9,26 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./assets/css/'));
 });
 
+gulp.task('coffee', function() {
+  gulp.src('./assets/coffee/**/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(gulp.dest('./assets/coffee/compiled/'));
+});
+
+gulp.task('concat', function() {
+
+  gulp.src('./assets/coffee/compiled/controllers/*.js')
+    .pipe(concat('controllers.js'))
+    .pipe(gulp.dest('./assets/js/'));
+
+  gulp.src('./assets/coffee/compiled/app/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./assets/js/'));
+
+
+});
+
 gulp.task('watch', function () {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
-  gulp.watch('./views/**/*.html', ['views']);
+  gulp.watch('./assets/coffee/**/*.coffee', ['coffee', 'concat']);
 });
