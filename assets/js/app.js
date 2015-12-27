3ffc836148782882fe8ea6b35417277e2fd76d81
Chris Harvey <chris@chrisnharvey.com>
2015-12-27 09:31:27 +0000

    Adding compiled app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,17 @@
-$(function() {
-    const remote = require('remote');
-    const dialog = remote.require('dialog');
-    const blk = remote.require('linux-blockutils');
+var installer, installerControllers;
 
-    $('.ladda-button').ladda('bind');
+installer = angular.module('installer', ['ngRoute', 'installerControllers', 'ladda']);
 
-    blk.getBlockInfo({}, function(err,json) {
-      if (err) {
-          dialog.showErrorBox('An error occurred', 'Unable to list your disks');
-      } else {
-         $.each(json, function(key, disk) {
-             if (disk.TYPE !== 'disk') return;
+installerControllers = angular.module('installerControllers', []);
 
-             $('#select-disk').append($('<option />').val(disk.NAME).text(disk.MODEL));
-         });
-      }
+installer.config([
+  '$routeProvider', function($routeProvider) {
+    return $routeProvider.when('/', {
+      templateUrl: './pages/welcome.html',
+      controller: 'homeController'
+    }).when('/disk', {
+      templateUrl: './pages/disk.html',
+      controller: 'diskController'
     });
-
-    $('#disk-util').click(function() {
-        require('child_process').spawn('gnome-disks');
-    });
-})
+  }
+]);
